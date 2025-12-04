fe51c508d227edd8b1bbe1e8056c5ecf643634b7
Nabeel-Zawia <devnabeelkhalid@gmail.com>
2025-12-04 08:23:28 +0300

    added selection sort

---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+function mergeSort(arr){
+
+    if(arr.length <=1){
+        return arr
+    }
+
+    const mid = Math.floor(arr.length / 2)
+    const left = arr.slice(0,mid)
+    const right = arr.slice(mid)
+    
+    const sortedLeft = mergeSort(left)
+    const sortedRight = mergeSort(right)
+
+    return merge(sortedLeft,sortedRight)
+}
+
+function merge(left,right){
+    let result = []
+    while (left.length > 0 && right.length > 0){
+        if(left[0] <= right[0] ){
+            result.push(left.shift())
+        }
+        else{
+            result.push(right.shift())
+        }
+    }
+
+    result.push(...left)
+    result.push(...right)
+    return result
+}
+
+let array = [4,3,2,1,6,7]
+let result = mergeSort(array)
+console.log(result)
